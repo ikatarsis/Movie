21f02349a3b846e60366af55a9df1a2466236a20
ekaterina.shevchenko <ekaterina.shevchenko@axenix.pro>
2026-04-18 20:58:21 +0300

    Initial Commit

---
+++ Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie
+//
+//  Created by ekaterina.shevchenko on 18.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by ekaterina.shevchenko on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
